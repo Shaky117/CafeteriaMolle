b54205f1133520f8c6210bf9175564872dfcd5d0
Maestro <Maestro@iMac-de-Maestro.local>
2018-04-23 18:48:47 -0700

    Pedidos Usuario

---
+++ app/EstadoPedido.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EstadoPedido extends Model
+{
+    protected $table = 'estadosPedidos';
+    public $timestamps = false;
+}
